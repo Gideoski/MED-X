9803e22ad39a3204455cff31c08ca4b6639ef731
Gideon Jackbara <gideonjackbara@gmail.com>
2026-02-20 23:55:03 +0000

    public/images/MED-X logo.jpeg this is the logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 const Logo = ({ className }: { className?: string }) => {
   return (
     <img
-      src="/logo.svg"
+      src="/images/MED-X logo.jpeg"
       alt="MED-X Logo"
       className={cn("h-8 w-8", className)}
     />
